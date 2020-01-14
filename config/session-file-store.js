20b192af7db487c9338419dfd2ec8f30b705c2c7
Braden Gommi <bgommi@hotmail.com>
2020-01-14 12:58:52 -0800

    Update session-file-store.js

--- config/session-file-store.js
+++ config/session-file-store.js
@@ -3,6 +3,6 @@ const fileStore = require('session-file-store')
 module.exports = (session) => {
 	fileStoreSession = fileStore(session)
 	return new fileStoreSession({
-		path: './sessions'
+		path: __basedir + '/sessions'
 	})
 }
