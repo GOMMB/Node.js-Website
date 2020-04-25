b6f6d0ea413fd674195d0952de52f550f04fb6e8
Braden Gommi <bgommi@hotmail.com>
2020-04-25 15:53:28 -0700

    added online list

--- config/socketio.js
+++ config/socketio.js
@@ -1,6 +1,19 @@
+let online = []
+
 module.exports = (io) => {
 	io.on('connection', (socket) => {
 		let uname = socket.handshake.session.uname
+
+		online.push(uname)
+
+		socket.emit('initialList', online)
+
+		socket.broadcast.emit('personConnected', uname)
+
+		socket.on('disconnect', () => {
+			online.splice(online.indexOf(uname), 1)
+			socket.broadcast.emit('personDisconnected', uname)
+		})
 
 		socket.on('message', (data) => {
 			data.uname = uname
